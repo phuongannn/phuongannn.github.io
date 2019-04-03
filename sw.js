53aaa6137f7f58fc3bee591557da37a0e7ae210e
Phuong-An N <phuongannn@gmail.com>
2019-04-02 23:26:52 -0700

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6b21e286d2d82b11c2c4.js"
+    "url": "webpack-runtime-96e966ffd6174b0a44be.js"
   },
   {
     "url": "app-2a0fbe88dd72ad8640d2.js"
@@ -36,7 +36,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "c7d61d281f14aec5949aec560b6c7f4e"
+    "revision": "d01f2813010248bb5b6c58f942d43d78"
   },
   {
     "url": "static/d/520/path---offline-plugin-app-shell-fallback-a-30-c5a-NZuapzHg3X9TaN1iIixfv1W23E.json"
